78e99619a1e79951d47e2a8cbbcff06955e4714d
RdEErAqUa <rdeeraqion@gmail.com>
2020-10-09 14:01:50 +0300

    Добавленая базовая инфрастуктура и ViewModel

---
+++ Quau/ViewModels/Base/ViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Quau.ViewModels.Base
+{
+    internal abstract class ViewModel : INotifyPropertyChanged, IDisposable
+    {
+        #region Шаблон события изменение елемента
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string PropertyName = null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(PropertyName);
+            return true;
+
+        }
+        #endregion
+
+        #region Деструктор
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
