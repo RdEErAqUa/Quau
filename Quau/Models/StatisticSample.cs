b60950291737389df0b13c93c090956ac0bc4d26
RdEErAqUa <rdeeraqion@gmail.com>
2020-10-09 15:28:43 +0300

    Добавлена модель выборки

---
+++ Quau/Models/StatisticSample.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quau.Models
+{
+    internal class StatisticSample
+    {
+        //Одновимірна вибірка
+        public ICollection<double> Sample { get; set; }
+
+        public SamplePrimaryStatisticAnalyse SamplePrimaryAnalyse { get; set; }
+    }
+
+    internal class SampleRanking
+    {
+        // xl
+        public ICollection<double> SampleData { get; set; }
+        //nl
+        public ICollection<int> SampleDataFrequency { get; set; }
+        //pl
+        public ICollection<double> SampleDataRelativeFrequency { get; set; }
+    }
+
+    internal class SamplePrimaryStatisticAnalyse
+    {
+        public SampleRanking SampleDataRanking { get; set; }
+    }
+
+    internal class SamplePrimaryDivisionINClass
+    {
+        public ICollection<double> SampleDivisionData { get; set; }
+
+        public ICollection<double> SampleDivisionDataFrequency { get; set; }
+
+        public ICollection<double> SampleDivisionDataRelativeFrequency { get; set; }
+    }
+}
---
+++ Quau/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Quau.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quau.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        public MainWindowViewModel()
+        {
+        }
+    }
+}
